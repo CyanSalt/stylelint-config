c918ab3b5f6581c79175d69b59473541957848cb
CyanSalt <c.salt@hotmail.com>
2021-03-08 17:58:40 +0800

    fix: use stylelint-config-recommended

--- base.js
+++ base.js
@@ -2,35 +2,13 @@
  * Supports stylelint@^13.12.0
  */
 module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+  ],
   rules: {
     /** Possible errors */
-    'color-no-invalid-hex': true,
-    'font-family-no-duplicate-names': true,
-    // 'font-family-no-missing-generic-family-keyword': null,
-    'named-grid-areas-no-invalid': true,
-    'function-calc-no-invalid': true,
-    'function-calc-no-unspaced-operator': true,
-    'function-linear-gradient-no-nonstandard-direction': true,
-    'string-no-newline': true,
-    'unit-no-unknown': true,
-    'property-no-unknown': true,
-    'keyframe-declaration-no-important': true,
-    'declaration-block-no-duplicate-custom-properties': true,
-    'declaration-block-no-duplicate-properties': [true, { ignore: ['consecutive-duplicates'] }],
-    'declaration-block-no-shorthand-property-overrides': true,
-    'block-no-empty': true,
-    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep', 'export', 'global'] }],
-    'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['v-deep', 'v-global'] }],
-    'selector-type-no-unknown': true,
-    'media-feature-name-no-unknown': true,
-    'at-rule-no-unknown': true,
-    'comment-no-empty': true,
-    // 'no-descending-specificity': null,
-    'no-duplicate-at-import-rules': true,
-    'no-duplicate-selectors': true,
-    'no-empty-source': true,
-    'no-extra-semicolons': true,
-    'no-invalid-double-slash-comments': true,
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-descending-specificity': null,
 
     /** Limit language features */
     'color-named': 'always-where-possible',

956b1a5237f636eb25b0218a870098e6e82d9d33
CyanSalt <c.salt@hotmail.com>
2021-06-28 11:47:15 +0800

    feat: add stylelint-plugin-aurora

--- aurora/index.js
+++ aurora/index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { hasInstalledPackage } = require('./utils')
+const { hasInstalledPackage } = require('../utils')
 
 const presets = [
   path.resolve(__dirname, './base.js'),
@@ -7,10 +7,6 @@ const presets = [
 
 if (hasInstalledPackage('stylelint-scss')) {
   presets.push(path.resolve(__dirname, './scss.js'))
-}
-
-if (hasInstalledPackage('stylelint-plugin-aurora')) {
-  presets.push(path.resolve(__dirname, './aurora/index.js'))
 }
 
 module.exports = {
---
+++ aurora/scss.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'aurora/selector-nested-combinator-position': ['as-prefix', { includes: ['::v-deep'] }],
+  },
+}

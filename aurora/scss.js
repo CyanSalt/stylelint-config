d36c7418fdf75f50a5f80db6681741f5b52ecf42
CyanSalt <c.salt@hotmail.com>
2022-01-29 11:15:24 +0800

    docs: add comment

--- aurora/scss.js
+++ aurora/scss.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
+    // 选择器组合符在嵌套时必须作为内部的前缀使用
     'aurora/selector-nested-combinator-position': ['as-prefix', { includes: ['::v-deep'] }, { severity: 'warning' }],
   },
 }

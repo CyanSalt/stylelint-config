49bbee199ca4bfc2bb3c84300defaf68b84c1387
CyanSalt <c.salt@hotmail.com>
2023-05-09 14:23:08 +0800

    fix: add inset order

--- order/groups.js
+++ order/groups.js
@@ -7,6 +7,13 @@ const content = [
 
 const positioning = [
   'position',
+  'inset',
+  'inset-block',
+  'inset-block-start',
+  'inset-block-end',
+  'inset-inline',
+  'inset-inline-start',
+  'inset-inline-end',
   'top',
   'right',
   'bottom',

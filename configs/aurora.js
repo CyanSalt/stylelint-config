06f6d8793351b9b7045a62e1640a8f87d5af0ab0
CyanSalt <c.salt@hotmail.com>
2025-02-07 11:14:41 +0800

    fix: severity config

--- configs/aurora.js
+++ configs/aurora.js
@@ -14,7 +14,7 @@ export default defineConfig(options => {
         files: [GLOB_ALL],
         rules: {
           // 选择器组合符在嵌套时必须作为内部的前缀使用
-          'aurora/selector-nested-combinator-position': ['as-prefix', { includes: ['::v-deep'] }, { severity: 'warning' }],
+          'aurora/selector-nested-combinator-position': ['as-prefix', { includes: ['::v-deep'], severity: 'warning' }],
         },
       },
     ] : []),

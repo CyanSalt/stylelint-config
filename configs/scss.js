0d0b6fe3407ae408f06fe8f9828a4dfa7922feda
CyanSalt <c.salt@hotmail.com>
2025-08-25 10:38:08 +0800

    fix: avoid parsing errors

--- configs/scss.js
+++ configs/scss.js
@@ -1,5 +1,5 @@
 import { defineConfig } from '../config.js'
-import { GLOB_ALL, GLOB_SCSS } from '../globs.js'
+import { GLOB_ALL, GLOB_SCSS, GLOB_VUE } from '../globs.js'
 
 export default defineConfig(options => {
   if (!options.scss) return []
@@ -10,19 +10,9 @@ export default defineConfig(options => {
       customSyntax: 'postcss-scss',
     },
     {
-      name: '@cyansalt/scss/setup',
-      files: [GLOB_ALL],
-      plugins: [
-        'stylelint-scss',
-      ],
+      name: '@cyansalt/scss/stylelint',
+      files: [GLOB_SCSS, GLOB_VUE],
       rules: {
-        /** 覆盖默认规则 */
-        // 允许 SCSS 的 at-rule
-        'at-rule-no-unknown': null,
-        'scss/at-rule-no-unknown': true,
-        // 支持 SCSS 的注释
-        'comment-no-empty': null,
-        'scss/comment-no-empty': true,
         // 允许 @use 和 @forward 出现在 @import 之前
         'no-invalid-position-at-import-rule': [true, { ignoreAtRules: ['use', 'forward'] }],
         // 允许 !default 标注
@@ -50,29 +40,52 @@ export default defineConfig(options => {
         }],
         // 允许在 media query 值中插入 SCSS 表达式
         'media-feature-name-value-no-unknown': null,
-
-        // 自动删除 @function 函数名后的空格
-        'scss/at-function-parentheses-space-before': ['never', { severity: 'warning' }],
-        // 自动删除 @mixin Mixin 名后的空格
-        'scss/at-mixin-parentheses-space-before': ['never', { severity: 'warning' }],
-        // 自动优化空格于变量的冒号后
-        'scss/dollar-variable-colon-space-after': ['always', { severity: 'warning' }],
-        'scss/dollar-variable-colon-space-before': ['never', { severity: 'warning' }],
-        // 必须在单行注释 // 的后面添加空格
-        'scss/double-slash-comment-whitespace-inside': ['always', { severity: 'warning' }],
-        // 禁用属性嵌套功能
-        'scss/declaration-nested-properties': 'never',
-        // 操作符换行时，必须出现在下一行开头
-        'scss/operator-no-newline-after': true,
-        // 操作符两侧必须添加空格
-        'scss/operator-no-unspaced': true,
-        // 禁止在同一个块内重复定义变量
-        'scss/no-duplicate-dollar-variables': [true, { ignoreInside: ['at-rule'] }],
-        // 禁止重复导入规则
-        'scss/no-duplicate-load-rules': true,
-        // 禁止在同一个块内重复定义 Mixin
-        'scss/no-duplicate-mixins': true,
       },
     },
+    ...(options.scss ? [
+      {
+        name: '@cyansalt/scss/setup',
+        files: [GLOB_ALL],
+        plugins: [
+          'stylelint-scss',
+        ],
+        rules: {
+          // 自动删除 @function 函数名后的空格
+          'scss/at-function-parentheses-space-before': ['never', { severity: 'warning' }],
+          // 自动删除 @mixin Mixin 名后的空格
+          'scss/at-mixin-parentheses-space-before': ['never', { severity: 'warning' }],
+          // 自动优化空格于变量的冒号后
+          'scss/dollar-variable-colon-space-after': ['always', { severity: 'warning' }],
+          'scss/dollar-variable-colon-space-before': ['never', { severity: 'warning' }],
+          // 必须在单行注释 // 的后面添加空格
+          'scss/double-slash-comment-whitespace-inside': ['always', { severity: 'warning' }],
+          // 禁用属性嵌套功能
+          'scss/declaration-nested-properties': 'never',
+          // 操作符换行时，必须出现在下一行开头
+          'scss/operator-no-newline-after': true,
+          // 操作符两侧必须添加空格
+          'scss/operator-no-unspaced': true,
+          // 禁止在同一个块内重复定义变量
+          'scss/no-duplicate-dollar-variables': [true, { ignoreInside: ['at-rule'] }],
+          // 禁止重复导入规则
+          'scss/no-duplicate-load-rules': true,
+          // 禁止在同一个块内重复定义 Mixin
+          'scss/no-duplicate-mixins': true,
+        },
+      },
+      {
+        name: '@cyansalt/scss/superset',
+        files: [GLOB_SCSS, GLOB_VUE],
+        rules: {
+          /** 覆盖默认规则 */
+          // 允许 SCSS 的 at-rule
+          'at-rule-no-unknown': null,
+          'scss/at-rule-no-unknown': true,
+          // 支持 SCSS 的注释
+          'comment-no-empty': null,
+          'scss/comment-no-empty': true,
+        },
+      },
+    ] : []),
   ]
 })
